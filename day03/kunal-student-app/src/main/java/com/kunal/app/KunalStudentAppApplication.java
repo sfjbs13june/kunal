17925c4f524ad7903086ca0d913232b7517f684f
kunal <kunal@mail.com>
2023-09-06 08:07:09 +0000

    Spring security

---
+++ day03/kunal-student-app/src/main/java/com/kunal/app/KunalStudentAppApplication.java
@@ -0,0 +1,14 @@
+package com.kunal.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication()
+public class KunalStudentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunalStudentAppApplication.class, args);
+	}
+
+}
