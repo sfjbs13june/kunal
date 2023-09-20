5cd99459a67422304ca8307bdc2b08f9683e1ca4
kunal <kunal@mail.com>
2023-09-20 10:54:38 +0000

    hospital app with mysql and test cases

---
+++ day14/hospital-application-mysql/src/main/java/com/kunal/app/repository/DataRepository.java
@@ -0,0 +1,26 @@
+package com.kunal.app.repository;
+
+import com.kunal.app.model.Patient;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class DataRepository {
+
+    private Map<String, Patient> patientMap= new HashMap<String,Patient>();
+
+    public Patient getPatient(String name){
+        return patientMap.get(name);
+    }
+
+    public void savePatient(String name,Patient patient){
+         patientMap.put(name,patient);
+    }
+
+    public void removePatient(String name){
+         patientMap.remove(name);
+    }
+
+}
